1cd13f6c90221480abb027158a071b8eabda962e
Udon <udoooon0727@gmail.com>
2017-12-01 09:26:13 +0900

    first commit

---
+++ TongueTongueRevolution/Assets/Scripts/GameManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	public GameObject[] notes;
+	private float[] _timing;
+	private int[] _lineNum;
+
+	public string filePass;
+	private int _notesCount = 0;
+
+	public AudioSource _audioSource;
+	private float _startTime = 0;
+
+	public float timeOffset = -1;
+
+	private bool _isPlaying = false;
+
+	public Text scoreText;
+	public static int score = 0;
+	public string musicTitle = "";
+	public static int dx = 0, dy = 0;
+
+	public float timeThreshord = 20;
+	private float timeNow;
+	private float timeOrigin;
+
+	// Use this for initialization
+	void Start () {
+		musicTitle = GlobalObjects.getInstance().StringParam;
+		_audioSource = GameObject.Find ("GameMusic").GetComponent<AudioSource> ();
+		_audioSource.clip = Resources.Load ("Musics/" + musicTitle) as AudioClip;
+		_timing = new float[1024];
+		_lineNum = new int[1024];
+		LoadCSV ();
+		StartGame ();
+		timeOrigin = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isPlaying) {
+			CheckNextNotes ();
+			scoreText.text = score.ToString ();
+		}
+		//Debug.Log ();
+
+		if (Time.time - timeOrigin> timeThreshord) {
+			Debug.Log ("GameEnd");
+			gotoResult();
+		}
+
+	}
+
+	public void StartGame(){
+		_startTime = Time.time;
+		_audioSource.Play ();
+		Debug.Log (Application.dataPath + "/Musics/" + musicTitle);
+		_isPlaying = true;
+	}
+
+	//ゲーム開始時間とnotesを落とす時間を比較して，落とす時間を過ぎていればnotesを落とす
+	void CheckNextNotes(){
+//		if (_notesCount > _lineNum.Length) {
+//			//gotoResult ();
+//			Debug.Log("countend");
+//		}
+		while (_timing [_notesCount] + timeOffset < GetMusicTime () && _timing [_notesCount] != 0) {
+			SpawnNotes (_lineNum[_notesCount]);
+			_notesCount++;
+		}
+	}
+
+
+	void SpawnNotes(int num){
+		Instantiate (notes[num], 
+			new Vector3 (-1.3f + (1.0f * num), 10.0f, 0),
+			Quaternion.identity);
+	}
+
+	//notesを落とすべき時間と落とすnotesを読み込む
+	//timingに落とす時間
+	//lineNumに矢印の方向
+	void LoadCSV(){
+		int i = 0, j;
+		filePass = "CSV/" + musicTitle;
+		TextAsset csv = Resources.Load (filePass) as TextAsset;
+		StringReader reader = new StringReader (csv.text);
+		while (reader.Peek () > -1) {
+
+			string line = reader.ReadLine ();
+			string[] values = line.Split (',');
+			for (j = 0; j < values.Length; j++) {
+				_timing [i] = float.Parse( values [0] );
+				_lineNum [i] = int.Parse( values [1] );
+			}
+			i++;
+			Debug.Log (i);
+		}
+	}
+
+	float GetMusicTime(){
+		return Time.time - _startTime;
+	}
+
+	public void GoodTimingFunc(int num){
+		Debug.Log ("good" + num);
+		score++;
+	}
+
+	public void setDxDy(int x, int y){
+		dx = x;
+		dy = y;
+	}
+
+	public int getDx(){
+		return dx;
+	}
+	public int getDy(){
+		return dy;
+	}
+
+	private void gotoResult(){
+		SceneManager.LoadScene ("ResultScene");
+	}
+
+
+}
---
+++ TongueTongueRevolution/Assets/Scripts/NotesScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesScript : MonoBehaviour {
+	public int lineNum;
+	private GameManager _gameManager;
+	int dx ,dy;
+
+	// Use this for initialization
+	void Start () {
+		_gameManager = GameObject.Find ("GameManager").GetComponent<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += Vector3.down * 5 * Time.deltaTime;
+		if (this.transform.position.y < -5.0f) {
+			//Debug.Log("false");
+			//false
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnTriggerStay(Collider other){
+		switch (lineNum) {
+		case 0:
+			//CheckInput (KeyCode.D);
+			//left
+			dx = _gameManager.getDx ();
+			dy = _gameManager.getDy ();
+			if (dx < 0 && dy > 0) {
+				Debug.Log ("↑ or ←");
+				goodTongue ();
+			} else if (dx < 0 && dy < 0) {
+				Debug.Log ("← or ↓");
+				goodTongue ();
+			}
+			break;
+		case 1:
+			//CheckInput (KeyCode.F);
+			//up
+			dx = _gameManager.getDx ();
+			dy = _gameManager.getDy ();
+			if (dx > 0 && dy < 0) {
+				Debug.Log ("→ or ↑");
+				goodTongue ();
+			} else if (dx < 0 && dy > 0) {
+				Debug.Log ("↑ or ←");
+				goodTongue ();
+			}
+			break;
+		case 2:
+			//CheckInput (KeyCode.Space);
+			//down
+			dx = _gameManager.getDx ();
+			dy = _gameManager.getDy ();
+			if (dx > 0 && dy > 0) {
+				Debug.Log ("→ or ↓");
+				goodTongue ();
+			}else if (dx < 0 && dy < 0) {
+				Debug.Log ("← or ↓");
+				goodTongue ();
+			}
+			break;
+		case 3:
+			//CheckInput (KeyCode.J);
+			//right
+			dx = _gameManager.getDx ();
+			dy = _gameManager.getDy ();
+			if (dx > 0 && dy > 0) {
+				Debug.Log ("→ or ↓");
+				goodTongue ();
+			} else if (dx > 0 && dy < 0) {
+				Debug.Log ("→ or ↑");
+				goodTongue ();
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	void goodTongue(){
+		_gameManager.GoodTimingFunc (lineNum);
+		Destroy (this.gameObject);
+	}
+}
